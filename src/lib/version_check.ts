cc67272773cf0fe48447c84b9b05baaa8229da5d
Wilson <wilsoncheung007@yahoo.com.hk>
2025-08-28 20:24:54 +0800

    Update version_check.ts

--- src/lib/version_check.ts
+++ src/lib/version_check.ts
@@ -13,7 +13,7 @@ export enum UpdateStatus {
 
 // 远程版本检查URL配置
 const VERSION_CHECK_URLS = [
-  'https://github.com/wilsonx123/LunaTV/blob/main/VERSION.txt',
+  'https://raw.githubusercontent.com/wilsonx123/LunaTV/refs/heads/main/VERSION.txt',
 ];
 
 /**
